16ebb179fe383cc046b3c3aa328c6c19c2ae03f0
vn55c9p <tyler.reitz@walmart.com>
2025-07-11 15:21:30 -0700

    filter for ages >= 21

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,7 +2,7 @@ const User = require('../models/User')
 
 const getUserById = async (req, res) => {
   try {
-    const user = await User.findById(req.params.id);
+    const user = await User.findOne({ _id: req.params.id, age: { $gte: 21}})
     if (!user) return res.status(404).json({ message: 'User not found' })
 
     res.json(user)
